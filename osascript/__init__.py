fdb5d0eba0e072d2d727209f58705e11dd52bc74
zwimer <github@zwimer.com>
2026-08-12 15:23:24 -0400

    Allow str or bytes returns

--- osascript/__init__.py
+++ osascript/__init__.py
@@ -1,5 +1,5 @@
 from .osascript import run
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
-__all__ = ("__version__", "run")
+__all__ = ("__version__", "run", "run_bytes")
--- osascript/osascript.py
+++ osascript/osascript.py
@@ -1,23 +1,36 @@
 from tempfile import NamedTemporaryFile
 from pathlib import Path
+from typing import cast
 import subprocess
 
-__all__ = ("run",)
+__all__ = ("run", "run_bytes")
 
 
-def _run(path: str | Path) -> tuple[int, bytes, bytes]:
-    p = subprocess.run(["osascript", path], capture_output=True, check=False)
+def _sub_run(path: str | Path, text: bool) -> tuple[int, str, str] | tuple[int, bytes, bytes]:
+    p = subprocess.run(["osascript", path], text=text, capture_output=True, check=False)
     return p.returncode, p.stdout, p.stderr
 
 
-def run(cmd: str | Path) -> tuple[int, bytes, bytes]:
-    """
-    Execute the given applescript command or file
-    Return the (return code, stdout, and stderr)
-    """
+def _run(cmd: str | Path, text: bool) -> tuple[int, str, str] | tuple[int, bytes, bytes]:
     if isinstance(cmd, Path):
-        return _run(cmd)
+        return _sub_run(cmd, text)
     with NamedTemporaryFile("w", delete_on_close=False) as f:
         f.write(cmd)
         f.close()
-        return _run(f.name)
+        return _sub_run(f.name, text)
+
+
+def run(cmd: str | Path) -> tuple[int, str, str]:
+    """
+    Execute the given applescript command or file (run with subprocess' text=True)
+    Return the (return code, stdout, and stderr)
+    """
+    return cast(tuple[int, str, str], _run(cmd, True))
+
+
+def run_bytes(cmd: str | Path) -> tuple[int, bytes, bytes]:
+    """
+    Execute the given applescript command or file (run with subprocess' text=False)
+    Return the (return code, stdout, and stderr)
+    """
+    return cast(tuple[int, bytes, bytes], _run(cmd, False))
